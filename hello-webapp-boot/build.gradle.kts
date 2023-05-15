2ae6b892e2df40b17b0eeebaa5a6f1c379218701
Dongsu Jang <77693+iolo@users.noreply.github.com>
2023-05-15 13:31:00 +0900

    hello-webapp-boot: replace jackson into gson

--- hello-webapp-boot/build.gradle.kts
+++ hello-webapp-boot/build.gradle.kts
@@ -12,7 +12,15 @@ repositories {
 	mavenCentral()
 }
 
+configurations {
+  all {
+    exclude(group="com.fasterxml.jackson.core", module="jackson-databind") // ObjectMapper 클래스가 있는 의존성 제외
+  }
+}
+
 dependencies {
+  implementation("com.google.code.gson:gson") // Gson 클래스가 있는 의존성 추가(버전은 지정할 필요없음)
+  implementation("org.springframework.boot:spring-boot-starter-json");
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
