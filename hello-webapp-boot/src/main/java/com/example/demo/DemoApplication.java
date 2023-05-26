654113837f685fedc97282728294df505515c834
Dongsu Jang <77693+iolo@users.noreply.github.com>
2023-05-26 10:02:55 +0900

    hello-webapp-boot: add cors mapping

--- hello-webapp-boot/src/main/java/com/example/demo/DemoApplication.java
+++ hello-webapp-boot/src/main/java/com/example/demo/DemoApplication.java
@@ -2,21 +2,34 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Controller
 public class DemoApplication {
 
-	@RequestMapping("/")
-	public ModelAndView greetings(@RequestParam(defaultValue="world") String name) {
-		return new ModelAndView("greetings", "name", name);
-	}
+    @RequestMapping("/")
+    public ModelAndView greetings(@RequestParam(defaultValue = "world") String name) {
+        return new ModelAndView("greetings", "name", name);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Bean
+    public WebMvcConfigurer webMvcConfiguration() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
